a6e35b0dbc1e347686fa34ad25b2fb84af10dc0f
Farrux <fkgmc@.ru>
2023-03-02 22:36:33 +0000

    ver1

---
+++ main.js
@@ -0,0 +1,29 @@
+var imya = prompt ('ваше имя')
+console.log('ваше имя ' + imya);
+var age = prompt ('ваш возраст')
+console.log('ваш возраст ' + age);
+
+
+
+
+
+var word = prompt ('сколько будет 10 + 5 =?')
+console.log('Правильный ответ ' + (10 + 5) +' Ваш ответ '  + word);
+var word2 = prompt ('сколько будет 10 - 5 =?')
+console.log('Правильный ответ ' + (10 - 5) +' Ваш ответ '  + word2);
+var word3 = prompt ('сколько будет 10 * 5 =?')
+console.log('Правильный ответ '  + 10 * 5 +' Ваш ответ '  + word3);
+var word4 = prompt ('сколько будет 10 / 5 =?')
+console.log('Правильный ответ ' + 10 / 5 +' Ваш ответ '  + word4);
+var word5 = prompt ('сколько будет 10 % 5 =?')
+console.log('Правильный ответ ' + 10 % 5 +' Ваш ответ '  + word5);
+
+
+var nuber1= + prompt ("Введите первое число: ");
+var nuber2= + prompt ("Введите второе число: ");
+var nuber3= + prompt ("Введите третье число: ");
+ 
+
+alert((nuber1 + nuber2 + nuber3) / 3)
+
+
